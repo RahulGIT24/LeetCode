840b74e99128e476e1598f5f0f33fdab736a6ae2
Rahul Gupta <103873656+RahulGIT24@users.noreply.github.com>
2023-06-23 17:21:12 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (56.11%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int red = 0;
+        int white = 0;
+        int blue = 0;
+        
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] == 0) {red++;}
+            if(nums[i] == 1) {white++;}
+            if(nums[i] == 2) {blue++;}
+        }
+        
+        for(int i = 0; i<nums.length; i++){
+            if(i<red){
+                nums[i] = 0;
+            }else if(i<red+white){
+                nums[i] = 1;
+            }else{
+                nums[i] = 2;
+            }
+        }
+    }
+}
