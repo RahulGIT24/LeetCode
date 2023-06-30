c1f6866a3da94c33f18e03fc352d2e72772aab21
Rahul Gupta <103873656+RahulGIT24@users.noreply.github.com>
2023-06-30 10:11:21 +0530

    Time: 5 ms (12.28%), Space: 41.8 MB (65.95%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -10,21 +10,13 @@ class Solution {
     }
     
     void sort(int[] arr){
-        int temp;
-        boolean swap;
-        for(int i = 0; i<arr.length; i++){
-            swap = false;
-            for(int j = 1; j<arr.length-i; j++){
-                if(arr[j-1] > arr[j]){
-                    temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
-                    swap = true;
+        for(int i = 0; i<arr.length-1; i++){
+            for(int j = i+1; j>0; j--){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
                 }
-            }
-            
-            if(!swap){
-                break;
             }
         }
     }
