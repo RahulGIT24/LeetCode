5aef65b8f4aa089991206ed1be81283abb0b845c
Rahul Gupta <103873656+RahulGIT24@users.noreply.github.com>
2023-09-02 15:19:53 +0530

    Time: 7 ms (51.15%), Space: 53.6 MB (42.18%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,29 +1,31 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-       sort(nums);
-
-        ArrayList<Integer> list = new ArrayList<>();
-for (int i = 0; i< nums.length; i++){
-            if(i+1 != nums[i]){
+        sort(nums);
+        
+        ArrayList<Integer> list =  new ArrayList<Integer>();
+        
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] != i+1){
                 list.add(nums[i]);
             }
         }
-
-       return list;
+        
+        return list;
     }
     
-void sort(int[] arr){
+    void sort(int[] arr){
         int i = 0;
-        while (i < arr.length){
+        while(i<arr.length){
             int correctIndex = arr[i] - 1;
-            if(arr[i] != arr[correctIndex]){
+            if(arr[correctIndex] != arr[i]){
                 swap(arr, i, correctIndex);
             }else{
                 i++;
             }
         }
     }
-void swap(int[] arr, int start, int end){
+    
+    void swap(int[] arr, int start, int end){
         int temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
