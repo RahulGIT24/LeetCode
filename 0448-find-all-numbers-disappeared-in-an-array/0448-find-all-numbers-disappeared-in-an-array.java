6582439e49ea0adab83056dbeffdb7d4f792751d
Rahul Gupta <103873656+RahulGIT24@users.noreply.github.com>
2023-09-02 15:09:14 +0530

    Time: 6 ms (67.85%), Space: 53.2 MB (72.38%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,23 +1,22 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         sort(nums);
-
-        ArrayList<Integer> list = new ArrayList<>();
         
-        for (int i = 0; i < nums.length; i++) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        for(int i = 0; i<nums.length; i++){
             if(nums[i] != i+1){
                 list.add(i+1);
             }
         }
         
         return list;
-        
     }
     
-void sort(int[] arr){
+    void sort(int[] arr){
         int i = 0;
-        while (i < arr.length){
-            int correctIndex = arr[i] - 1;
+        while(i<arr.length){
+            int correctIndex = arr[i]-1;
             if(arr[i] != arr[correctIndex]){
                 swap(arr, i, correctIndex);
             }else{
@@ -25,7 +24,8 @@ void sort(int[] arr){
             }
         }
     }
-void swap(int[] arr, int start, int end){
+    
+    void swap(int[] arr, int start, int end){
         int temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
