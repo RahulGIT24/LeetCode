c02df423834fc733362c1b6c461e84c2d4e0b107
Rahul Gupta <103873656+RahulGIT24@users.noreply.github.com>
2023-10-12 17:37:10 +0530

    Time: 34 ms (33.02%), Space: 19.4 MB (48.77%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,22 +6,12 @@ public:
         int n = intervals.size();
             
         for(int i = 0; i<n; i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(!ans.empty() && end<= ans.back()[1]){
-                continue;
+            if(ans.empty() || intervals[i][0]>ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }else{
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
-            
-            for(int j = i+1; j<n; j++){
-                if(intervals[j][0] <=end){
-                    end = max(end, intervals[j][1]);
-                }else{
-                break;
-                }
-            }
-            ans.push_back({start, end});
         }
-        
         return ans;
     }
 };
