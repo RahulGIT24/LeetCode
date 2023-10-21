5584e6e52f0bfb8eccafa3eb3ef4717f8c2cd952
Rahul Gupta <103873656+RahulGIT24@users.noreply.github.com>
2023-10-21 20:39:24 +0530

    Time: 31 ms (83.36%), Space: 20.3 MB (9.37%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+int findMax(vector<int> &v) {
+    int maxi = INT_MIN;
+    int n = v.size();
+    //find the maximum:
+    for (int i = 0; i < n; i++) {
+        maxi = max(maxi, v[i]);
+    }
+    return maxi;
+}
+
+double calculateTotalHours(vector<int> &v, int hourly) {
+    double totalH = 0;
+    int n = v.size();
+    //find total hours:
+    for (int i = 0; i < n; i++) {
+        totalH += ceil((double)(v[i]) / (double)(hourly));
+    }
+    return totalH;
+}
+
+int minimumRateToEatBananas(vector<int> v, int h) {
+    int low = 1, high = findMax(v);
+
+    //apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        double totalH = calculateTotalHours(v, mid);
+        if (totalH <= h) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        return minimumRateToEatBananas(piles,h);
+    }
+};
