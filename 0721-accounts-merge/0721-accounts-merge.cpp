97bf43369f883538a894225e2c0ee6bfbab386e6
Rahul Gupta <103873656+RahulGIT24@users.noreply.github.com>
2024-02-16 15:01:36 +0530

    Time: 75 ms (62.37%), Space: 37.8 MB (50.77%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,76 @@
+class DisjointSet{
+    public:
+    vector<int> parent;
+    vector<int> size;
+    
+    DisjointSet(int n){
+        parent.resize(n+1,0);
+        size.resize(n+1,1);
+        for(int i = 0; i<=n; i++){
+            parent[i] = i;
+        }
+    }
+    
+    int findParent(int node){
+        if(parent[node] == node) {
+            return node;
+        }
+        return parent[node] = findParent(parent[node]);
+    }
+    
+    void unionBySize(int u, int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n = accounts.size();
+        DisjointSet ds(n);
+        sort(accounts.begin(), accounts.end());
+        unordered_map<string, int> mapMailNode;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < accounts[i].size(); j++) {
+                string mail = accounts[i][j];
+                if (mapMailNode.find(mail) == mapMailNode.end()) {
+                    mapMailNode[mail] = i;
+                }
+                else {
+                    ds.unionBySize(i, mapMailNode[mail]);
+                }
+            }
+        }
+
+        vector<string> mergedMail[n];
+        for (auto it : mapMailNode) {
+            string mail = it.first;
+            int node = ds.findParent(it.second);
+            mergedMail[node].push_back(mail);
+        }
+
+        vector<vector<string>> ans;
+
+        for (int i = 0; i < n; i++) {
+            if (mergedMail[i].size() == 0) continue;
+            sort(mergedMail[i].begin(), mergedMail[i].end());
+            vector<string> temp;
+            temp.push_back(accounts[i][0]);
+            for (auto it : mergedMail[i]) {
+                temp.push_back(it);
+            }
+            ans.push_back(temp);
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
